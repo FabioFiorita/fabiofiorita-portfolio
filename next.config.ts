2fa8feace268257d44eda95e39b6c8d6ffa3c945
Fabio Fiorita <fabiolfp@gmail.com>
2025-06-23 10:22:15 -0300

    feat: add global styles and layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	i18n: {
+		locales: ["en", "pt"],
+		defaultLocale: "en",
+	},
 };
 
 export default nextConfig;
